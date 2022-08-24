381f4eb4bd1a4b787db6c65fb4c25e52a1d78395
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-08-24 18:29:30 -0400

    models.comment

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const commentSchema = require('./Comment');
 const dateFormat = require('../utils/dateFormat');
 
 const blogSchema = new Schema(
@@ -19,7 +19,7 @@ const blogSchema = new Schema(
       type: String,
       required: true
     },
-    reactions: [reactionSchema]
+    comments: [commentSchema]
   },
   {
     toJSON: {
@@ -28,8 +28,8 @@ const blogSchema = new Schema(
   }
 );
 
-blogSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
+blogSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
 });
 
 const Blog = model('Blog', blogSchema);
