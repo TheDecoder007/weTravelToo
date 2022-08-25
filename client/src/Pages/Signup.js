b22607bb064762fe64549b67c0682fb279a621eb
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-08-25 19:51:09 -0400

    added in an event and set form state into signup.js

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -14,10 +14,27 @@ const Signup = () => {
 
 // update state based on form input changes
 const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
 };
 
 // submit form
 const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
 };
 
 return (
