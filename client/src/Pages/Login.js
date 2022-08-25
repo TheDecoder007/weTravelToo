cd4c8b3efc4c9028baeac5874e530d49fe748563
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-08-25 19:32:41 -0400

    created a 'submit form' and 'clear form values'

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -11,6 +11,32 @@ const Login = (props) => {
     // update state based on form input changes
     const handleChange = (event) => {
       const { name, value } = event.target;
+    
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+};
+
+// submit form
+const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+}
+
+// clear form values
+setFormState({
+    email: '',
+    password: '',
+  });
 };
 
 return (
