8e6c6b13583a9de43a3b50b8ff4d71a28d41596b
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-08-28 18:23:46 -0400

    got buttons to hide based on loggedin authentication, started on single page commenting ability, auth

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,29 +5,44 @@ import Row from "react-bootstrap/Row";
 import BlogList from "../components/BlogList";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { QUERY_BLOGS, QUERY_ME_BASIC } from "../utils/queries";
-import Auth from "../utils/auth";
+
 
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_BLOGS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const blogs = data?.blogs || [];
-  // const loggedIn = Auth.loggedIn();
+  const loggedIn = Auth.loggedIn();
 
   return (
- 
+    
     <Container fluid className="projectCont" id="blogs">
       <Row className="sectionTopRow">
         <h3 className="text-center sectionHead">Recent Blogs</h3>
+        {!loggedIn && (
         <Button className="AllBtn">
         <Link to="/signup">Signup</Link>
        </Button>
+       )}
+       {!loggedIn && (
        <Button className="AllBtn">
        <Link to="/login">Login</Link>
        </Button>
-      </Row>
+       )}
+      {loggedIn && (
+      <Button className="AllBtn">
+       <Link to="/CreateBlog">Create-Blog</Link>
+       </Button>
+       )}
+         {loggedIn && (
+      <Button className="AllBtn">
+       <Link to="/CreateBlog">Logout</Link>
+       </Button>
+       )}
+       </Row>
       <Row className="CardRow">
   <BlogList blogs={blogs} title="Some Feed for Blog(s)..." />
         
