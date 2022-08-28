b90783ea3e802e74b81e28b8099d720b9b353afd
Ryan Murphy <TheDecoder007@gmail.com>
2022-08-28 19:57:43 -0400

    login, logout, signup working. create blog page fails to load. profile fails to recognize logged in

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -10,47 +10,67 @@ import Auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { QUERY_BLOGS, QUERY_ME_BASIC } from "../utils/queries";
 
-
-
 const Home = () => {
   const { loading, data } = useQuery(QUERY_BLOGS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const blogs = data?.blogs || [];
-  const loggedIn = Auth.loggedIn();
+  // const loggedIn = Auth.loggedIn();
+
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
   return (
     
     <Container fluid className="projectCont" id="blogs">
+    {Auth.loggedIn() ? (
+      <>
       <Row className="sectionTopRow">
-
+          <Col>
+            <Button className="AllBtn HomeBtn">
+              <Link to="/profile">My Profile</Link>
+            </Button>
+          </Col>
+          <Col>
         <h3 className="text-center sectionHead">Recent Blogs</h3>
-        {!loggedIn && (
-        <Button className="AllBtn">
-        <Link to="/signup">Signup</Link>
-       </Button>
-       )}
-       {!loggedIn && (
-       <Button className="AllBtn">
-       <Link to="/login">Login</Link>
-       </Button>
-       )}
-      {loggedIn && (
-      <Button className="AllBtn">
-       <Link to="/CreateBlog">Create-Blog</Link>
-       </Button>
-       )}
-         {loggedIn && (
-      <Button className="AllBtn">
-       <Link to="/CreateBlog">Logout</Link>
-       </Button>
-       )}
-       </Row>
+          </Col>
+          <Col>
+            <Button className="AllBtn HomeBtn">
+              <a href="/" onClick={logout}>
+                Log Out
+              </a>
+            </Button>
+        </Col>
+        
+        </Row>
+        
+        </>
+        ) : (
+          <>
+
+      <Row className="sectionTopRow">
+          <Col>
+            <Button className="AllBtn HomeBtn">
+              <Link to="/signup">Signup</Link>
+            </Button>
+          </Col>
+          <Col>
+            <h3 className="text-center sectionHead">Recent Blogs</h3>
+          </Col>
+          <Col>
+            <Button className="AllBtn HomeBtn">
+              <Link to="/login">Login</Link>
+            </Button>
+          </Col>
+      </Row>
+          </>
+        )}
 
       <Row className="CardRow">
-  <BlogList blogs={blogs} title="Some Feed for Blog(s)..." />
-        
+        <BlogList blogs={blogs} title="Some Feed for Blog(s)..." />
       </Row>
-    </Container>
+      </Container>
   );
 };
 
