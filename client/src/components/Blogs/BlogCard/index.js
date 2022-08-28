0f2b08792036c7639d9487f90d1c6b3c2e463565
Ryan Murphy <TheDecoder007@gmail.com>
2022-08-28 12:50:28 -0400

    slight changes is blog index

--- client/src/components/Blogs/BlogCard/index.js
+++ client/src/components/Blogs/BlogCard/index.js
@@ -4,8 +4,8 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 
-const BlogCards = ({ blogs}) => {
-  if (!blogs.length) {
+const BlogCards = ({ Blog }) => {
+  if (!Blog.length) {
     return <h3>No Blogs Yet</h3>;
   }
 
@@ -17,12 +17,12 @@ return (
       className="text-center wholeCard"
       style={{ width: "16rem", height: "25rem" }}
     >
-      <Card.Img src={blog.blogImage} alt={alt} />
+      <Card.Img src={Blog.blogImage} alt={alt} />
 
       <Card.Body>
-        <div className="cardTitle">{blog.blogTitle}</div>
-        <Card.Text>{blog.username}</Card.Text>
-        <Card.Text>{blog.blogDescription}</Card.Text>
+        <div className="cardTitle">{Blog.blogTitle}</div>
+        <Card.Text>{Blog.username}</Card.Text>
+        <Card.Text>{Blog.blogDescription}</Card.Text>
         <>
         </>
       </Card.Body>
